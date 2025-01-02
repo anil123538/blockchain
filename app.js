bba082cc3903078d5b55ab0a62ad6424d200c9b1
Anil Bhattarai <146853404+anil123538@users.noreply.github.com>
2025-01-02 07:03:53 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import { ethers } from 'ethers';
+
+import web3 from 'web3';
 const contractAddress = "0xC5E0e0b25c9B4b4a5f9c3Ed6e1f13E3e88f8e034"; // Replace with your contract address
 const abi = [ [
 	{
